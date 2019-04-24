122d346655db8fdff1d5a84bc0e146a36ca21640
Diana <felix.diana@uabc.edu.mx>
2019-04-23 19:07:08 -0700

    Proyecto APPWBD

---
+++ src/main/java/com/appwbd/sraeu/SraeuApplication.java
@@ -0,0 +1,13 @@
+package com.appwbd.sraeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SraeuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SraeuApplication.class, args);
+    }
+
+}
