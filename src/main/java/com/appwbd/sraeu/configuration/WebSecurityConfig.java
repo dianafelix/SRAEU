63d4ae0aa1e60ea1b7a2ca8011e405902082f039
sagaan <salgadoa@uabc.edu.mx>
2019-05-27 14:51:45 -0700

    Security 1.0

--- src/main/java/com/appwbd/sraeu/configuration/WebSecurityConfig.java
+++ src/main/java/com/appwbd/sraeu/configuration/WebSecurityConfig.java
@@ -26,19 +26,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        /*http
-            .authorizeRequests()
-                .antMatchers().permitAll()
-                .anyRequest().authenticated()
-            .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-            .and()
-                .logout()
-                .permitAll();*/
         http
             .authorizeRequests()
+                .antMatchers("/eventos/showEventos", "/asistents/showAsistentes", "/lugares/showLugares").hasAuthority("CONSULTAR")
+                .antMatchers("/eventos/eventoForm", "/asistents/asistenteForm", "/lugares/lugarForm").hasAnyAuthority("REGISTRAR", "MODIFICAR")
+                .antMatchers("/eventos/removeEvento", "/asistentes/removeAsistente", "/lugares/removeLugar").hasAuthority("ELIMINAR")
+                .antMatchers("/usuarios/*").hasAuthority("MANTENIMIENTOU")
                 .anyRequest().permitAll()
             .and().formLogin()
                 .loginPage("/login").permitAll()
